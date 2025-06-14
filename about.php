f13688ea5944c7c3c201f237e479c3711fa75ab5
Nimanthika <nimanthikaabeyrathna@gmail.com>
2025-06-13 22:34:03 -0700

    add privacy and policy page

--- about.php
+++ about.php
@@ -9,25 +9,25 @@
 </head>
 <body class="bg-gray-50 text-gray-800 font-sans">
 
-<?php include "nav.php" ?>
+    <?php include "nav.php" ?>
 
-<div class="max-w-3xl mx-auto p-6 mt-12 space-y-8 text-center">
-  
-  <h1 class="text-4xl font-bold text-indigo-700">About This Blog</h1>
-  
-  <img src="./images/myphoto.png" alt="" class="mx-auto rounded-full w-40 h-40 object-cover shadow-lg border-4 border-indigo-300"/>
-  
-  <p class="text-lg italic text-gray-600 leading-relaxed">
-    “This blog is where I share my book reviews, science experiments, and fun learning tips for kids like me!”
-  </p>
-  
-  <p class="text-md text-gray-700">
-    “Feel free to explore and comment on your favorite posts!”
-  </p>
-  
-</div>
+    <div class="max-w-3xl mx-auto p-6 mt-12 space-y-8 text-center">
+    
+    <h1 class="text-4xl font-bold text-indigo-700">About This Blog</h1>
+    
+    <img src="./images/myphoto.png" alt="" class="mx-auto rounded-full w-40 h-40 object-cover shadow-lg border-4 border-indigo-300"/>
+    
+    <p class="text-lg italic text-gray-600 leading-relaxed">
+        “This blog is where I share my book reviews, science experiments, and fun learning tips for kids like me!”
+    </p>
+    
+    <p class="text-md text-gray-700">
+        “Feel free to explore and comment on your favorite posts!”
+    </p>
+    
+    </div>
 
-<?php include "footer.php" ?>
+    <?php include "footer.php" ?>
 
 </body>
 </html>
--- privacy.php
+++ privacy.php
@@ -4,8 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PrivacyAndPolicy</title>
+     <link rel="icon" type="image/png" href="./images/favicon.png" />
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
+    <?php include "nav.php" ?>
+
+    <div class="max-w-3xl mx-auto px-6 py-35">
+        <h1 class="text-3xl font-bold text-indigo-700 mb-6">Privacy Policy</h1>
+
+        <div class="space-y-4 text-lg">
+            <p>This website does not collect personal information.</p>
+            <p>If you have any questions, please contact us at 
+                <a href="mailto:contentsphere@gmail.com" class="text-indigo-600 underline">contentsphere@gmail.com</a>.
+            </p>
+        </div>
+        
+        <p class="text-sm text-gray-500 mt-8">Last updated: June 14, 2025</p>
+  </div>
+
+
+    <?php include "footer.php" ?>
     
 </body>
 </html>
