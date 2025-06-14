a2cbd62f8cac6337cc7ac04825423b5d80cf393c
Nimanthika <nimanthikaabeyrathna@gmail.com>
2025-06-13 22:14:32 -0700

    add contact page

--- contact.php
+++ contact.php
@@ -4,8 +4,56 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact</title>
+    <link rel="icon" type="image/png" href="./images/favicon.png" />
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
+    <?php include "nav.php"  ?>
+   <div class="max-w-4xl mx-auto px-6 py-12 space-y-10">
+
+  <h1 class="text-4xl font-bold text-center text-indigo-700 mb-8">Get in Touch</h1>
+
+  <div class="grid md:grid-cols-2 gap-8">
+    <div class="space-y-4">
+      <div>
+        <h2 class="text-xl font-semibold">📞 Phone Number</h2>
+        <p class="text-gray-700">081-3589874</p>
+      </div>
+
+      <div>
+        <h2 class="text-xl font-semibold">📧 Email</h2>
+        <p class="text-gray-700">contentsphere@gmail.com</p>
+      </div>
+
+      <div>
+        <h2 class="text-xl font-semibold">🏠︎ Address</h2>
+        <p class="text-gray-700">Rathnapura</p>
+      </div>
+
+      <div>
+        <h2 class="text-xl font-semibold">⏰ Open Hours</h2>
+        <p class="text-gray-700">Any time</p>
+      </div>
+    </div>
+
+    <div class="rounded-lg overflow-hidden shadow-lg border border-gray-200">
+      <iframe 
+        src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d1981.3031250714384!2d80.39733599126664!3d6.695592410690755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNsKwNDEnNDQuMSJOIDgwwrAyMyc1Mi45IkU!5e0!3m2!1sen!2slk!4v1749876918386!5m2!1sen!2slk" 
+        width="100%" 
+        height="100%" 
+        class="w-full h-80"
+        style="border:0;" 
+        allowfullscreen="" 
+        loading="lazy" 
+        referrerpolicy="no-referrer-when-downgrade">
+      </iframe>
+    </div>
+  </div>
+
+</div>
+
+
+    <?php include "footer.php"  ?> 
     
 </body>
 </html>
